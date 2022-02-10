da21ccd062aac20ddba1db11bdb656d5119749f1
Roberto Cajueiro <beto.cajueiro@gmail.com>
2022-02-10 12:15:03 -0300

    First commit

---
+++ typescript/app.ts
@@ -0,0 +1,10 @@
+let mensagem: string = "Seja bem vindo!!!"
+let temporadasFriends: number = 10
+let estudandoAngular: boolean = true
+let listaDeFrutas: Array<string> = ['Uva', 'Banana', 'Abacaxi']
+let notasDasProvas: Array<number> = [7.5, 8, 9]
+
+let listaDeFrutas2: string[] = ['Uva', 'Banana', 'Abacaxi']
+let notasDasProvas2: number[] = [7.5, 8, 9]
+
+
