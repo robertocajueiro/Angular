50224b0ded9a9b8f3e39e09d970c807ffae01714
Roberto Cajueiro <beto.cajueiro@gmail.com>
2022-03-31 13:24:10 -0300

    update app1

---
+++ app1/src/app/painel/painel.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Frase } from '../shared/frase.model'
+import { FRASES } from './frases-mock'
+
+@Component({
+  selector: 'app-painel',
+  templateUrl: './painel.component.html',
+  styleUrls: ['./painel.component.css']
+})
+export class PainelComponent implements OnInit {
+
+  public frases: Frase[] = FRASES
+  public instrucao: string = 'Traduza a frase:'
+  public resposta: string | undefined
+
+  constructor() { console.log(this.frases)}
+
+  ngOnInit(): void {
+  }
+
+  public atualizaResposta(resposta: Event ): void {
+    this.resposta = ((<HTMLInputElement>resposta.target).value)
+    console.log(this.resposta)
+  }
+
+}
