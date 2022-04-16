982521f9e2568ad97d3eb71729814dbc2f68da16
robertocajueiro <beto.cajueiro@gmail.com>
2022-04-16 12:49:44 -0300

    125. App1 - Controlando a exibição do Painel e testando o método ngOnDestroy

--- app1/src/app/painel/painel.component.ts
+++ app1/src/app/painel/painel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
 
 import { Frase } from '../shared/frase.model'
 import { FRASES } from './frases-mock'
@@ -8,7 +8,7 @@ import { FRASES } from './frases-mock'
   templateUrl: './painel.component.html',
   styleUrls: ['./painel.component.css']
 })
-export class PainelComponent implements OnInit {
+export class PainelComponent implements OnInit, OnDestroy {
 
   public frases: Frase[] = FRASES
   public instrucao: string = 'Traduza a frase:'
@@ -27,7 +27,11 @@ export class PainelComponent implements OnInit {
     this.atualizaRodada()
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+      console.log('Componente painel foi destruido!')
   }
 
   public atualizaResposta(resposta: Event ): void {
