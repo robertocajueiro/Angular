75dbe6b8f487fd1a60e0a2cc27cc15a53150de43
robertocajueiro <beto.cajueiro@gmail.com>
2022-04-15 21:11:55 -0300

    110. App1 - Verificando resposta

--- app1/src/app/painel/painel.component.ts
+++ app1/src/app/painel/painel.component.ts
@@ -14,7 +14,12 @@ export class PainelComponent implements OnInit {
   public instrucao: string = 'Traduza a frase:'
   public resposta: string | undefined
 
-  constructor() { console.log(this.frases)}
+  public rodada: number = 0
+  public rodadaFrase!: Frase
+
+  constructor() { this.rodadaFrase = this.frases[this.rodada]
+  console.log(this.rodadaFrase)
+  }
 
   ngOnInit(): void {
   }
@@ -22,6 +27,24 @@ export class PainelComponent implements OnInit {
   public atualizaResposta(resposta: Event ): void {
     this.resposta = ((<HTMLInputElement>resposta.target).value)
     console.log(this.resposta)
+  }
+
+  public verificarResposta(): void {
+
+    if(this.rodadaFrase.frasePtBr == this.resposta) {
+            alert('A tradução está correta!')
+
+      //trocar a pergunta da rodada
+      this.rodada++
+
+      // atualiza o objeto rodadaFrase
+      this.rodadaFrase = this.frases[this.rodada]
+
+    } else {
+      alert('A tradução está errada!')
+    }
+
+
   }
 
 }
