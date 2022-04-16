b575d2bbf071e057a347c784b6bc05bef57a1a46
robertocajueiro <beto.cajueiro@gmail.com>
2022-04-15 22:01:13 -0300

    113. App1 - Atualizando a rodada

--- app1/src/app/painel/painel.component.ts
+++ app1/src/app/painel/painel.component.ts
@@ -12,15 +12,15 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES
   public instrucao: string = 'Traduza a frase:'
-  public resposta: string | undefined
+  public resposta: string = ''
 
   public rodada: number = 0
   public rodadaFrase!: Frase
 
   public progresso: number = 0
 
-  constructor() { this.rodadaFrase = this.frases[this.rodada]
-  console.log(this.rodadaFrase)
+  constructor() {
+    this.atualizaRodada()
   }
 
   ngOnInit(): void {
@@ -44,13 +44,21 @@ export class PainelComponent implements OnInit {
       console.log(this.progresso)
 
       // atualiza o objeto rodadaFrase
-      this.rodadaFrase = this.frases[this.rodada]
+      this.atualizaRodada()
 
     } else {
       alert('A tradução está errada!')
     }
 
+  }
 
+  public atualizaRodada(): void {
+
+    // define a frada da rodada com base em alguma logica
+    this.rodadaFrase = this.frases[this.rodada]
+
+    //limpar a rodada
+    this.resposta = ''
   }
 
 }
